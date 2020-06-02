202f291059f753867e3800f5eab14d7178ba0521
Siya Sharma <ajaysh73@gmail.com>
2020-06-02 16:52:43 -0400

    Updates

---
+++ precache-manifest.cc8bba70e64e6c9de68e3473a136d06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb2f4ce9bb892bbb238035e6842285d",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "05e701fe79e6bca631c9",
+    "url": "/pics/static/css/main.c17912fa.chunk.css"
+  },
+  {
+    "revision": "f45b8b70c9227c2d31cd",
+    "url": "/pics/static/js/2.30b02b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics/static/js/2.30b02b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e701fe79e6bca631c9",
+    "url": "/pics/static/js/main.0d0d4392.chunk.js"
+  },
+  {
+    "revision": "57500f9949d7428b819e",
+    "url": "/pics/static/js/runtime-main.dba25da1.js"
+  }
+]);
